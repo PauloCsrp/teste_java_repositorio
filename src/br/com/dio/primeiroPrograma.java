97feea3fcec3d6f35fe7f8da78f1c1882ddf0681
paulo chaves <paulocsr19p@gmail.com>
2023-02-22 19:12:17 -0300

    version 1.0

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+		System.out.println(gato);
+
+		class Livros {
+			private String nome;
+			private String npag;
+		}
+
+		Livros livro = new Livros();
+		System.out.println(livro);
+
+		int a = 5;
+		int b = 10;
+		System.out.println("Hello Word!!!!!!!!" + (a + b));
+
+	}
+
+}
